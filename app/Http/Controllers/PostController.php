947991919fc0c22adc02de5b63eb1f46efd6eb8c
nammPortatilhp <correportatilo@correo.com>
2025-03-12 15:34:18 -0500

    se termina configuracion del metodo store de autenticacion

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,12 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Routing\Controller; //Creo que esta es otra solucion
 
 class PostController extends Controller
 {
-    //
+    // se ejecuta cuando es instanciado este controlador
+ public function __construct(){
+    $this->middleware('auth');
+ }
 
     public function index(){
-        dd(Auth::user());  
+         return view('dashboard');
     }
 }

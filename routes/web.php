ae892b7ec611687c2ac6213402afc9b2b06c9838
nammPortatilhp <correportatilo@correo.com>
2025-03-12 16:33:07 -0500

    agregar  ruta del muro

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
@@ -11,3 +12,6 @@ Route::get('/', function () {
 
 Route::get('/crear-cuenta', [RegisterController::class, 'index'])->name('register');
 Route::post('/crear-cuenta', [RegisterController::class, 'store'])->name('register');
+
+//
+Route::get('/muro',[PostController::class, 'index'])->name('posts.index');

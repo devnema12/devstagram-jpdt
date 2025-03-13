33a9bb8164c6c413fc7311465f41ac83cffc3614
nammPortatilhp <correportatilo@correo.com>
2025-03-13 16:46:59 -0500

    mostrando enlacens de forma condicional

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,12 +23,33 @@
     <header class="p-5 border-b bg-white shadow">
         <div class="container mx-auto flex justify-between items-center">
           <a href="{{ route('home') }}">  <h1 class="text-3xl font-black"> Devstagram</h1></a>
-            <nav class="flex gap-4">
+
+          @auth
+              <nav class="flex gap-4">
+              
+                <a
+              
+                href="{{ route('register') }}" 
+                class="font-bold uppercase text-gray-600 text-sm"> 
+                 <span class="font-normal">
+                  Hola: {{ auth()->user()->username}}
+                  </span></a>
+                <a
+              
+                href="{{ route('register') }}" 
+                class="font-bold uppercase text-gray-600 text-sm">Cerrar sesion</a>
+            </nav>
+          @endauth
+
+          @guest
+              <nav class="flex gap-4">
                 <a href="/login" class="font-bold uppercase text-gray-600 text-sm">Login</a>
                 <a
                 href="{{ route('register') }}" 
                 class="font-bold uppercase text-gray-600 text-sm">Crear Cuenta</a>
-            </nav>
+            </nav>  
+          @endguest
+         
         </div>
     </header>
 

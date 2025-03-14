c6d05596f58585e12d6b0a4149969d34b7761125
nammPortatilhp <correportatilo@correo.com>
2025-03-14 10:38:34 -0500

    agregando soporte para post en cerrar sesion

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\LogoutController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
@@ -13,8 +14,10 @@ Route::get('/', function () {
 
 Route::get('/crear-cuenta', [RegisterController::class, 'index'])->name('register');
 Route::post('/crear-cuenta', [RegisterController::class, 'store'])->name('register');
+
 Route::get('/login',[LoginController::class, 'index'])->name('login');
 Route::post('/login',[LoginController::class, 'store'])->name('login');
+Route::post('/logout',[LogoutController::class, 'store'])->name('logout');
 
 //
 Route::get('/muro',[PostController::class, 'index'])->name('posts.index');

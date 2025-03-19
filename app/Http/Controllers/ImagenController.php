10dfcd3097164b6bc678e421a04d80345177dfc6
nammPortatilhp <correportatilo@correo.com>
2025-03-19 14:20:55 -0500

    agregadno la imagen el servidor y obteniendo nombre  de la img

--- app/Http/Controllers/ImagenController.php
+++ app/Http/Controllers/ImagenController.php
@@ -3,16 +3,38 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use Intervention\Image\Laravel\Facades\Image;
+
+use Intervention\Image\ImageManager;
+use Intervention\Image\Drivers\Gd\Driver;
 
 class ImagenController extends Controller
 {
     //
     public function store(Request $request)
     {
+        $manager = new ImageManager(new Driver());
         $input = $request->all();
         $imagen = $request->file('file');
+        //Para que no se repitan los numeros
+        $nombreImagen = Str::uuid() . "." . $imagen->extension();
 
+        //guardar la imagen al servidor
+        $imagenServidor = $manager->read($imagen);
+        //agregamos efecto a la imagen con intervention
+        $imagenServidor->scale(1000, 1000);
+        // la unidad de mide en PX 1= 1pixiel
 
-        return response()->json(['imagen' => $imagen->extension()]);
+        //Mover la imagen al servidor --> eso lo guarda en public
+        $imagenPath = public_path('uploads') . '/' . $nombreImagen;
+
+        // para que guarde la imagen en esa ruta
+        $imagenServidor->save($imagenPath);
+
+        return response()->json([
+            'imagen' => $nombreImagen,
+
+        ]);
     }
 }

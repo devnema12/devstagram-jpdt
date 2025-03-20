0e74457697ff6c3de9539bf9e3f5552c27480826
nammPortatilhp <correportatilo@correo.com>
2025-03-20 10:25:18 -0500

    agregando test con factory e ingresando datos automaticamnte

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -18,6 +18,10 @@ class PostFactory extends Factory
     {
         return [
             //
+            'titulo' => $this->faker->sentence(5),
+            'description' => $this->faker->sentence(20),
+            'imagen' => $this->faker->uuid() . '.jpg',
+            'user_id' =>  $this->faker->randomElement([1, 2]),
         ];
     }
 }

3f9cc8972e2b7f65e96b75a65c930f299bc74d1c
nammPortatilhp <correportatilo@correo.com>
2025-03-31 11:30:13 -0500

    consultando publicaciones o post de usuario

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -21,6 +21,12 @@ class PostController extends Controller
    // da render a Dashboard
    public function index(User $user)
    {
+
+      //Consultaar tabla de popts y filtar por el usuario que esta en la url
+      $posts = Post::where('user_id', $user->id)->get();
+
+      dd('usuario', $posts);
+
       // toma el modelo de usuario para consultar el usurname del usuario
 
       return view('dashboard', [
@@ -37,22 +43,36 @@ class PostController extends Controller
    //Almacena en la base de datos
    public function store(Request $request)
    {
-      //Para validar los campos 
+      //1. Para validar los campos 
       $request->validate([
          'titulo' => 'required|max:255',
          'description' => 'required',
          'imagen' => 'required',
       ]);
 
-      // para crear el registro en la bd
-      Post::create([
+      //2.  para crear el registro en la bd
+      // Post::create([
+      //    'titulo' => $request->titulo,
+      //    'description' => $request->description,
+      //    'imagen' => $request->imagen,
+      //    'user_id' => Auth::user()->id,
+      // ]);
+
+      //2.  para crear el registro en la bd utilizando las relaciones
+      /*Como tenemos request obtenemos el usarioa actual con user() , lusgo llamamos la relacion de posts()
+      luego creamos los datos
+      */
+
+      $request->user()->posts()->create([
          'titulo' => $request->titulo,
          'description' => $request->description,
          'imagen' => $request->imagen,
          'user_id' => Auth::user()->id,
       ]);
 
-      //redirecionar
+      //3. el fillable en el modelo
+
+      //4. edirecionar
       return redirect()->route('posts.index', Auth::user()->username);
    }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
@@ -17,4 +18,11 @@ class Post extends Model
         'imagen',
         'user_id',
     ];
+
+    //1. crear Metodo de realcion donde un posts pertenece a un usuario
+    public function user()
+    {
+        //2. un posts pertenece a un usaurio
+        return $this->BelongsTo(User::class)->select(['name', 'username']);
+    }
 }

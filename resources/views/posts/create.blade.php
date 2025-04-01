659f534c897e8a5872ccef25f9049571fd321717
nammPortatilhp <correportatilo@correo.com>
2025-04-01 15:51:14 -0500

    agregando la caja de comentarios

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -9,66 +9,50 @@
 @endsection
 
 @section('contenido')
-
-<div class="md:flex md:items-center">
-    <div class="md:w-1/2 px-10">
-           <form 
-           action="{{ route('imagenes.store')}}" 
-           method="POST"
-           enctype="multipart/form-data"
-           id="dropzone" 
-           class="dropzone border-dashed border-2 w-full h-96 rounded flex flex-col justify-center items-center">
-           @csrf
-           </form>
-    </div> 
-    <div class=" md:w-1/2 bg-white p-6 rounded-lg  shadow-sm mt-10 md:mt-10">
-        <form action="{{ route('posts.store') }}" method="POST">
-            @csrf
-            <div class="mb-5">
-                <label for="titulo" class="mb-2 uppercase block text-gray-500 font-bold">Titulo:</label>
-                <input
-                    id="titulo"
-                    name="titulo"
-                    type="text"
-                    placeholder="Titulo de la publicacion"
-                    class="border p-3 w-full  rounded-lg @error('titulo') border-red-500                         
+    <div class="md:flex md:items-center">
+        <div class="md:w-1/2 px-10">
+            <form action="{{ route('imagenes.store') }}" method="POST" enctype="multipart/form-data" id="dropzone"
+                class="dropzone border-dashed border-2 w-full h-96 rounded flex flex-col justify-center items-center">
+                @csrf
+            </form>
+        </div>
+        <div class=" md:w-1/2 bg-white p-6 rounded-lg  shadow-sm mt-10 md:mt-10">
+            <form action="{{ route('posts.store') }}" method="POST">
+                @csrf
+                <div class="mb-5">
+                    <label for="titulo" class="mb-2 uppercase block text-gray-500 font-bold">Titulo:</label>
+                    <input id="titulo" name="titulo" type="text" placeholder="Titulo de la publicacion"
+                        class="border p-3 w-full  rounded-lg @error('titulo') border-red-500                         
                     @enderror"
-                    value="{{ old('titulo') }}"
-                />
-                @error('titulo')
-                    <p class="text-red-500 text-center mt-1">{{ str_replace('titulo', 'titulo', $message) }} </p>
-                @enderror
-            </div>
-           <div class=" mb-5">
-               
+                        value="{{ old('titulo') }}" />
+                    @error('titulo')
+                        <p class="text-red-500 text-center mt-1">{{ str_replace('titulo', 'titulo', $message) }} </p>
+                    @enderror
+                </div>
+                <div class=" mb-5">
+
                     <label for="description" class="mb-2 uppercase block text-gray-500 font-bold">description:</label>
-                    <textarea
-                        name="description"
-                        id="description"
-                        placeholder="description de la publicacion"
+                    <textarea name="description" id="description" placeholder="description de la publicacion"
                         class="border p-3 w-full  rounded-lg 
                         @error('description')
                          border-red-500                         
-                        @enderror"   
-                        >{{ old('description') }}</textarea>
-                @error('description')
-                    <p class="text-red-500 text-center mt-1">{{ str_replace('description', 'descripcion', $message) }} </p>
-                @enderror
-            
-           </div>
+                        @enderror">{{ old('description') }}</textarea>
+                    @error('description')
+                        <p class="text-red-500 text-center mt-1">{{ str_replace('description', 'descripcion', $message) }} </p>
+                    @enderror
 
-           <div class="mb-5">
-                <input type="hidden" name="imagen"  value='{{ old('imagen')}}'>
-                @error('imagen')
-                <p class="text-red-500 text-center mt-1">{{ str_replace('imagen', 'imagen', $message) }} </p>
-            @enderror
-           </div>
+                </div>
 
-            <input
-             type="submit" 
-            value="Crear publicación" 
-            class="bg-sky-700 hover:bg-sky-700 transition-colors uppercase p-3 text-white rounded-lg w-full" >
-          </form>
+                <div class="mb-5">
+                    <input type="hidden" name="imagen" value='{{ old('imagen') }}'>
+                    @error('imagen')
+                        <p class="text-red-500 text-center mt-1">{{ str_replace('imagen', 'imagen', $message) }} </p>
+                    @enderror
+                </div>
+
+                <input type="submit" value="Crear publicación"
+                    class="bg-sky-700 hover:bg-sky-700 transition-colors uppercase p-3 text-white rounded-lg w-full">
+            </form>
+        </div>
     </div>
-</div>
 @endsection
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -5,4 +5,49 @@
 @endsection
 
 @section('contenido')
+    <div class="container mx-auto flex">
+
+        <div class="md:w-1/2">
+            <img src="{{ asset('uploads') . '/' . $post->imagen }}"
+                alt=" imagen de la publicacione de post {{ $post->titulo }}">
+
+            <div class="p-3">
+                <p>0 Likes</p>
+            </div>
+
+            <div>
+                <p class="font-bold ">{{ $post->user->username }}</p>
+                <p class="text-sm text-gray-600">{{ $post->updated_at->diffForHumans() }}</p>
+                <p class="mt-5"> <span class="font-bold">Descripción: </span>{{ $post->description }}</p>
+            </div>
+        </div>
+        <div class="md:w-1/2 p-5">
+            <div class="shadow mb-5 p-5 bg-white">
+                <p class="text-xl font-bold text-center m-4">Agrega un nuevo comentario</p>
+                <form action="">
+
+                    <div class="mb-5">
+                        <label for="comentario" class="mb-2 block upppercase text-gray-500 font-bold">Comentario</label>
+                        <textarea name="comentario" id="comentario" placeholder="comentario de la publicacion"
+                            class="border p-3 w-full rounded-lg 
+                             @error('comentario')
+                         border-red-500                         
+                        @enderror">{{ old('comentario') }}</textarea>
+
+
+
+                        {{-- @error('comentario')
+                            <p class="text-red-500 text-center mt-1">{{ $message }}</p>
+                        @enderror --}}
+
+                    </div>
+
+                    <input type="submit" value="Crear Comentario"
+                        class="bg-sky-600 hover:bg-sky-700 transition-colors cursor-pointer uppercase font-bold w-full p-3 text-white rounded-lg">
+                </form>
+            </div>
+        </div>
+
+
+    </div>
 @endsection

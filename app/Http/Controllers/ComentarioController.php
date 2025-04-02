e8609ff4e3484ffa75626ce94220858cf2024e7c
nammPortatilhp <correportatilo@correo.com>
2025-04-02 09:31:59 -0500

    agregando las columnas de la micgracion de comentario en el modelo en el meodo de fillable

--- app/Http/Controllers/ComentarioController.php
+++ app/Http/Controllers/ComentarioController.php
@@ -2,13 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Comentario;
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ComentarioController extends Controller
 {
     //
 
-    public function store(Request $request)
+    public function store(Request $request, User $user, Post $post)
     {
         //1. Validar
 
@@ -17,7 +21,16 @@ class ComentarioController extends Controller
         ]);
 
         //2. Almacenar
-
+        //2.1 Importar modelo de comentario
+        //2.2 usar funcion create
+        //2.3 el post_id biene de la url
+        //2.4 tomar el usuario autenticado que comento
+        //2.5 Agregar columnas en el fillable del modelo
+        Comentario::create([
+            'user_id' => Auth::user()->id,
+            'post_id' => $post->id,
+            'comentario' => $request->comentario,
+        ]);
 
 
 
--- app/Models/Comentario.php
+++ app/Models/Comentario.php
@@ -2,9 +2,18 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Comentario extends Model
 {
     //
+    use HasFactory;
+
+    //Para crear un registro, hay que crear el fillable con las columnas que se crearon en la tabla
+    protected $fillable = [
+        'user_id',
+        'post_id',
+        'comentario',
+    ];
 }

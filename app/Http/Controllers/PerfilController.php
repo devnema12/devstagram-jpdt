5fb10742cfeae637b09b2c894c040eff30a589cf
nammPortatilhp <correportatilo@correo.com>
2025-06-03 11:37:00 -0500

    agregando el middelware para proteger rutas de users no autenticados

--- app/Http/Controllers/PerfilController.php
+++ app/Http/Controllers/PerfilController.php
@@ -3,8 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Routing\Controller; //Creo que esta es otra solucion , para importar middleware
 
 class PerfilController extends Controller
 {
-    //
+    //proteger la ruta
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    //mostrar la vista
+    public function index()
+    {
+        // Solo usuarios logueados verán esto
+        dd('mostradno fomrulario');
+    }
 }

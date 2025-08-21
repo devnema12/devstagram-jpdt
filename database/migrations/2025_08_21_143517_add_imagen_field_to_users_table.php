055fa9dc9b20129bd399295ef13d24c5d9d7bb14
nammPortatilhp <correportatilo@correo.com>
2025-08-21 11:46:33 -0500

    realizando la migracion de columna imgaen

--- database/migrations/2025_08_21_143517_add_imagen_field_to_users_table.php
+++ database/migrations/2025_08_21_143517_add_imagen_field_to_users_table.php
@@ -12,8 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            //
-            $table->string('imagen');
+            // agregar la columna de imagen y nullable se le coloca para que pueda ir vacio esa columna
+            $table->string('imagen')->nullable();
         });
     }
 

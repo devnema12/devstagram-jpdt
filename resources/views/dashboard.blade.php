6e992739901f164f763661919ddb3e7854674841
nammPortatilhp <correportatilo@correo.com>
2025-09-03 09:37:44 -0500

    mostrando la im de perfil y los post

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,8 @@
     <div class="flex justify-center">
         <div class="w-full md:w-8/12 lg:w-6/12 flex flex-col items-center md:flex-row">
             <div class="w-8/12 lg:6/12 p-5 border border-spacing-1">
-                <img src="{{ asset('perfiles') . '/' . $user->imagen }}" alt="imagen_avatar_usuario">
+                <img src="{{ $user->imagen ? asset('perfiles') . '/' . $user->imagen : asset('img/usuario.svg') }}"
+                    alt="imagen_avatar_usuario" alt="imagen de perfil de usuario">
             </div>
             <div
                 class="md:w-8/12 lg:6/12 px-5 flex flex-col justify-center items-center md:justify-center md:items-start py-10 md:py-10">
@@ -38,7 +39,7 @@
                     <span class="font-normal">Siguiendo</span>
                 </p>
                 <p class="text-gray-800 text-sm mb-3 font-bold">
-                    0
+                    {{ $user->posts->count() }}
                     <span class="font-normal">Posts</span>
                 </p>
             </div>

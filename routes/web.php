89f53867f073651a522fd501bc9920b2313804ce
nammPortatilhp <correportatilo@correo.com>
2025-09-10 15:02:54 -0500

    corrigiendo rutas de sigueme

--- routes/web.php
+++ routes/web.php
@@ -53,5 +53,5 @@ Route::delete('/posts/{post}/likes', [LikeController::class, 'destroy'])->name('
 
 
 //Siguiendo Usuarios
-Route::post('/{user:username}/follow', [FollowerController::class], 'store')->name('users.follow');
-Route::delete('/{user:username}/unfollow', [FollowerController::class], 'destroy')->name('users.unfollow');
+Route::post('/{user:username}/follow', [FollowerController::class, 'store'])->name('users.follow');
+Route::delete('/{user:username}/unfollow', [FollowerController::class, 'destroy'])->name('users.unfollow');

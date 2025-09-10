48c71b6adf5b5409eab156ab94186b0841230695
nammPortatilhp <correportatilo@correo.com>
2025-09-10 15:29:30 -0500

    agregando el fotrmulario de seguir pero condicional

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -43,20 +43,22 @@
                     <span class="font-normal">Posts</span>
                 </p>
                 @auth
-                    <form action="" method="POST">
-                        @csrf
-                        <input type="submit"
-                            class="bg-blue-600 text-white uppercase rounded-lg px-3 py-1 text-xs font-bold cursor-pointer"
-                            value="Seguir">
+                    @if ($user->id !== auth()->user()->id)
+                        <form action="{{ route('users.follow', $user) }}" method="POST">
+                            @csrf
+                            <input type="submit"
+                                class="bg-blue-600 text-white uppercase rounded-lg px-3 py-1 text-xs font-bold cursor-pointer"
+                                value="Seguir">
 
-                    </form>
-                    <form action="" method="POST">
-                        @csrf
-                        <input type="submit"
-                            class="bg-red-600 text-white uppercase rounded-lg px-3 py-1 text-xs font-bold cursor-pointer"
-                            value="Dejar de Seguir">
+                        </form>
+                        <form action="" method="POST">
+                            @csrf
+                            <input type="submit"
+                                class="bg-red-600 text-white uppercase rounded-lg px-3 py-1 text-xs font-bold cursor-pointer"
+                                value="Dejar de Seguir">
 
-                    </form>
+                        </form>
+                    @endif
                 @endauth
             </div>
         </div>
